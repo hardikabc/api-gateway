d132bdb80a08e793d8f6531be026b9e7cdaeb055
hardikabc <49833428+hardikabc@users.noreply.github.com>
2025-05-24 15:55:24 +0530

    Update LogicExample.java

--- src/main/java/com/example/logic/LogicExample.java
+++ src/main/java/com/example/logic/LogicExample.java
@@ -4,6 +4,6 @@ public class LogicExample {
 
     public static void main(String args[]){
         System.out.println("Test Main");
-         System.out.println("Code Local");
+        System.out.println("Git pushed");
     }
 }
