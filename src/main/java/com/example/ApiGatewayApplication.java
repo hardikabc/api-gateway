7ed2afebb0bfb92d17d6b730ed65b658719aee45
hardikabc <mukeshmca.singh@gmail.com>
2025-05-24 16:58:33 +0530

    code push

--- src/main/java/com/example/ApiGatewayApplication.java
+++ src/main/java/com/example/ApiGatewayApplication.java
@@ -16,6 +16,7 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 
 		System.out.println("Mukesh Main from local");
+		System.out.println("New Logic Data");
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 	
--- src/main/java/com/example/logic/LogicExample.java
+++ src/main/java/com/example/logic/LogicExample.java
@@ -4,5 +4,6 @@ public class LogicExample {
 
     public static void main(String args[]){
         System.out.println("Logic modified from local");
+        System.out.println("New LogicExa DATA");
     }
 }
